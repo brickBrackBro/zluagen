bb88bae8224a0313b5c858ae3c1728d5dffde5c9
Jacob Stannix <jakestannix@gmail.com>
2024-08-23 17:38:27 -0600

    annotation enum outlined

--- build.zig
+++ build.zig
@@ -1,14 +1,30 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{});
-
-    _ = b.addModule("luagen", .{
+    const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseFast });
+    const lua_dep = b.dependency("lua", .{
+        .target = target,
+        .release = optimize == .ReleaseFast,
+    });
+    const lua_mod = lua_dep.module("lua");
+    const gen_mod = b.addModule("luagen", .{
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
+    gen_mod.addImport("lua", lua_mod);
+
+    const test_cmd = b.addTest(.{
+        .name = "gen_tests",
+        .optimize = optimize,
+        .target = target,
+        .root_source_file = b.path("src/root.zig"),
+    });
+    test_cmd.root_module.addImport("lua", lua_mod);
+    const exe_test_cmd = b.addRunArtifact(test_cmd);
+    const test_step = b.step("test", "run tests");
+    test_step.dependOn(&exe_test_cmd.step);
 }
--- src/root.zig
+++ src/root.zig
@@ -1,10 +1,41 @@
 const std = @import("std");
+const meta = std.meta;
 const testing = std.testing;
+const lua = @import("lua");
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+pub const CommentTags = union(enum) {
+    pub const Alias = struct {
+        name: []const u8,
+        type: []const u8,
+    };
+    pub const Field = struct {
+        name: []const u8,
+        type: []const u8,
+    };
+    pub const Param = struct {
+        name: []const u8,
+        type: []const u8,
+    };
+    alias: Alias,
+    class: []const u8,
+    field: Field,
+    meta,
+    param: Param,
+    @"return": []const u8,
+    type: []const u8,
+    pub fn emitTag(self: CommentTags, writer: anytype) !void {
+        switch (self) {
+            .meta => try writer.print("---@{s}\n\n", .{@tagName(self)}),
+            inline .class, .@"return", .type => |v, tag| try writer.print("---@{s} {s}\n", .{ @tagName(tag), v }),
+            inline else => |v, tag| try writer.print("---@{s} {s} {s}\n", .{ @tagName(tag), v.name, v.type }),
+        }
+    }
+};
+test "print meta" {
+    const tag: CommentTags = .meta;
+    var buff = std.ArrayList(u8).init(testing.allocator);
+    defer buff.deinit();
+    const writer = buff.writer();
+    try tag.emitTag(writer);
+    try testing.expectEqualStrings("---@meta\n\n", buff.items);
 }
